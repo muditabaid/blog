1663278a6b6f6f268c7718e76f3c9a6d14853e15
Mudita Baid <muditabaid@outlook.com>
2017-11-18 17:18:30 +0530

    add posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  root 'welcome#index'
+  #get 'welcome/index'
+  resources :posts
+  root "posts#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
